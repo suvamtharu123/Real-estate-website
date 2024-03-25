ae1f77bd46e7da26cdb46c0a114a41df6368c05b
Suvam Chaudhary <suvamraytharu76@gmail.com>
2024-03-25 13:26:59 +0545

    first commit

---
+++ src/SubComponents/HeroSection.jsx
@@ -0,0 +1,13 @@
+
+const HeroSection = () => {
+  return (
+    <>
+      <section id='hero'>
+        <h1>BE OUR GUEST</h1>
+        <p>LIVE LIKE A KING IN OUR BEST HOUSES</p>
+      </section>
+    </>
+  )
+}
+
+export default HeroSection
---
+++ src/SubComponents/Regions.jsx
@@ -0,0 +1,25 @@
+
+const Regions = () => {
+  return (
+    <>
+      <section id='regions'>
+        <h1>OUR REGIONS</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, accusantium quo! Expedita sint iusto quaerat excepturi velit similique.</p>
+        <div className="region_container">
+          <div className="card">
+            <img src="/region1.jpg" alt="mountains" />
+            <h2>Mountains</h2>
+            <p><span>90</span> Properties</p>
+          </div>
+          <div className="card">
+            <img src="/region2.jpg" alt="coastline" />
+            <h2>Coastline</h2>
+            <p><span>52</span> Properties</p>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Regions
